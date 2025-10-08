b45f4cb609c86d129e26767136f7550c286516b3
john <yohannesprog@gmail.com>
2025-10-08 18:34:36 +0300

    chore(repo): normalize line endings to LF

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import Services from "./pages/Services";
+import NotFoundPage from "./pages/NotFoundPage";
+import ServicePage from "./pages/ServicePage";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import OurWorks from "./components/OurWorks";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      {/* <Route path='/services' element={<Services />} /> */}
+      <Route path='/services/:id' element={<ServicePage />} />    
+      <Route path='/contact' element={<Contact />} />    
+      <Route path='/about' element={<About />} />
+
+      <Route path='*' element={<NotFoundPage />} />
+
+    </Route>
   )
-}
+);
 
-export default App
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
